373ff7147ece90aa4bdb405ad9d541575d63f3bd
f.prados <f.prados@alumnos.upm.es>
2024-05-13 22:47:27 +0200

    Updated Level and LevelReader

--- src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Level.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Level.java
@@ -1,5 +1,50 @@
-package es.upm.pproject.parkingjam.parking_jam.model;
+import java.util.Map;
+import java.util.Stack;
 
 public class Level {
+    int score;                  // Puntuación del n
+    //Stack movements;
+    Map<Character, Car> cars;   // Coches del tablero
+    char[][]board;              //Mapa con la representación de los vehículos con letras
+
+    public Level (char[][] board, Map<Character, Car> cars){
+        this.score=0;
+        this.board= board;
+        this.cars= cars;
+    }
+
+    public void moveCar(char car, Coordinates newCoordinates) throws InvalidMovementException {
+        checkMovementValidity(car, newCoordinates);
+        cars.get(car).setCoordinates(newCoordinates.getX(),newCoordinates.getY());
+
+    }
+
+	private boolean checkMovementValidity(char carChar, Coordinates newCoordinates) throws InvalidMovementException{
+        //TODO checkear validez del movimiento (free tiles)
+
+        Car car =cars.get(carChar);
+        Coordinates carCoordinates = car.getCoordinates();
+        int carLength = car.getLength();
+        char carOrientation = car.getOrientation();
+        if(carCoordinates.getX()== newCoordinates.getX() && carCoordinates.getY()== newCoordinates.getY()) // no se debe co
+            throw new SameMovementException();
+        if(carOrientation=='v' ){
+            if(carCoordinates.getX()!= newCoordinates.getX()){ //si quiere moverse a una X diferente estaria moviendose lateralmente == invalido
+                throw new InvalidMovementException();
+            }
+            boolean valid=true;
+            int goalY = newCoordinates.getY();
+            int currentY= carCoordinates.getY();
+            boolean goalIsUp = goalY - currentY < 0;
+            boolean goalIsDown = goalY - currentY > 0;
+            while(valid){
+
+            }
+        }
+		throw new InvalidMovementException();
+	}
+    
+    
 
 }
+
--- src/main/java/es/upm/pproject/parkingjam/parking_jam/model/LevelReader.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/LevelReader.java
@@ -1,5 +1,50 @@
-package es.upm.pproject.parkingjam.parking_jam.model;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LevelReader {
+	public static final String LEVEL_FILE_NAME_FORMAT = "src/main/resources/levels/level_%d.txt";
+	
+    public static char[][] readMap(int level) {
+        String fileName = LEVEL_FILE_NAME_FORMAT;
+		BufferedReader reader;
+		char[][] board = null;
+
+        try {
+            // Abrir el archivo
+            reader = new BufferedReader(new FileReader(String.format(fileName, level)));		
+
+            // Leer la primera línea que contiene el nombre del nivel
+            String levelName = reader.readLine();
+			
+			if(levelName != null) {
+				// Leer la segunda línea que contiene las dimensiones del tablero
+				String secondLine = reader.readLine();
+				if(secondLine != null) {
+				String[] dimensiones = secondLine.split(" ");
+				if(dimensiones.length == 2) {
+					int nRows = Integer.parseInt(dimensiones[0]);
+					int nColumns = Integer.parseInt(dimensiones[1]);
+					if(nRows >=3 && nColumns >= 3) {
+						// Leer las siguientes nRows líneas que contienen los elementos del tablero
+						board = new char[nRows][nColumns];
+						for (int i = 0; i < nRows; i++) {
+							String row = reader.readLine();
+							for (int j = 0; j < nColumns; j++) {
+								board[i][j] = row.charAt(j);
+							}
+						}
+					}
+					}
+				}
+			}
+
+            // Cerrar el archivo
+            reader.close();
+
+        } catch (Exception e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+		return board;
+    }
 
 }
