32f3766bf38eb4fef60c8e5a2c4d2608e2d38260
MORILLA SORLÍ, DAVID <david.morilla@alumnos.upm.es>
2024-05-14 21:08:33 +0200

    Update Level.java to add implementation of checkMovementValidity

--- src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Level.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Level.java
@@ -8,15 +8,19 @@ import es.upm.pproject.parkingjam.parking_jam.model.exceptions.SameMovementExcep
 import es.upm.pproject.parkingjam.parking_jam.utilities.Coordinates;
 
 public class Level {
-    int score;                  // Puntuación del n
+    private int score;                  // Puntuación del n
     //Stack movements;
-    Map<Character, Car> cars;   // Coches del tablero
-    char[][]board;              //Mapa con la representación de los vehículos con letras
+    private Map<Character, Car> cars;   // Coches del tablero
+    private char[][]board;              //Mapa con la representación de los vehículos con letras
+    private int nrows;
+    private int ncolumns;
 
-    public Level (char[][] board, Map<Character, Car> cars){
+    public Level (char[][] board,  int nrows, int ncolumns, Map<Character, Car> cars){ // necesitamos guardar el tam del tablero para checkear la validez de un movimiento mas adelante.
         this.score=0;
         this.board= board;
         this.cars= cars;
+        this.nrows=nrows;
+        this.ncolumns=ncolumns;
     }
 
     public void moveCar(char car, Coordinates newCoordinates) throws InvalidMovementException, SameMovementException {
@@ -25,29 +29,74 @@ public class Level {
 
     }
 
-	private boolean checkMovementValidity(char carChar, Coordinates newCoordinates) throws InvalidMovementException, SameMovementException{
+	private boolean checkMovementValidity(char carChar, Coordinates newCoordinates, int carLength) throws SameMovementException{
         //TODO checkear validez del movimiento (free tiles)
 
         Car car =cars.get(carChar);
         Coordinates carCoordinates = car.getCoordinates();
         int carLength = car.getLength();
         char carOrientation = car.getOrientation();
-        if(carCoordinates.getX()== newCoordinates.getX() && carCoordinates.getY()== newCoordinates.getY()) // no se debe co
+        if(carCoordinates.getX()== newCoordinates.getX() && carCoordinates.getY()== newCoordinates.getY()) // no se debe computar como movimiento si se quiere mover al mismo lugar (de momento lanzo excepcion porque tecnicamente es valido pero no se debe sumar 1 al contador de movimientos, es como una accion vacia, ya veremos como tratamos este caso especifico
             throw new SameMovementException();
-        if(carOrientation=='v' ){
-            if(carCoordinates.getX()!= newCoordinates.getX()){ //si quiere moverse a una X diferente estaria moviendose lateralmente == invalido
-                throw new InvalidMovementException();
+        if(carOrientation=='V' ){
+            if(carCoordinates.getX()!= newCoordinates.getX() || newCoordinates.getY()<0 || newCoordinates.getY()>= nrows){ //si quiere moverse a una X diferente estaria moviendose lateralmente == invalido, no puede moverse a una fila menor a 0 o mayor igual que el numero de filas (null pointer exc)
+                return false;
             }
             boolean valid=true;
             int goalY = newCoordinates.getY();
             int currentY= carCoordinates.getY();
             boolean goalIsUp = goalY - currentY < 0;
-            boolean goalIsDown = goalY - currentY > 0;
-            while(valid){
-
+            if(goalIsUp){
+                int i = 1; // si inicializamos a 0 comprobaria su actual posicion tambien que ya sabemos que es valida 
+                int limit = currentY - goalY;
+                while(valid && i<limit + 1){
+                    if( (board[carCoordinates.getX()][currentY - i] != ' ') && (board[carCoordinates.getX()][currentY + i] != '@'))
+                        valid=false;
+                    i++;
+                }
+            } else{ //goal is down
+                int i = carLength;
+                int limit = goalY - currentY;
+                while(valid && i<limit + carLength){
+                    if(board[carCoordinates.getX()][currentY + i] != '@')
+                        return true;
+                    if(board[carCoordinates.getX()][currentY + i] != ' ') && (board[carCoordinates.getX()][currentY + i] != '@') ){)
+                        valid=false;
+                    }
+                    i++;
+                }
             }
+            return valid;
+        } else { // caso orientation == 'H'
+            if(carCoordinates.getY()!= newCoordinates.getY() || newCoordinates.getX()<0 || newCoordinates.getX()>= ncolumns){ //si quiere moverse a una Y diferente estaria moviendose verticalmente == invalido, no puede moverse a una columna menor a 0 o mayor igual que el numero de columnas (null pointer exc)
+                return false;
+            }
+            boolean valid=true;
+            int goalX = newCoordinates.getX();
+            int currentX= carCoordinates.getX();
+            boolean goalIsLeft = goalX - currentX < 0;
+            if(goalIsLeft){
+                int i = 1; // si inicializamos a 0 comprobaria su actual posicion tambien que ya sabemos que es valida 
+                int limit = currentX - goalX;
+                while(valid && i<limit + 1){
+                    if( (board[currentX - i][carCoordinates.getY()] != ' ') && (board[currentX - i][carCoordinates.getY()] != '@'))
+                        valid=false;
+                    i++;
+                }
+            } else{ //goal is right
+                int i = carLength;
+                int limit = goalX - currentX;
+                while(valid && i<limit + carLength){
+                    if(board[currentX + i][carCoordinates.getY()] != '@')
+                        return true;
+                    if(board[currentX + i][carCoordinates.getY()] != ' ') && (board[currentX + i][carCoordinates.getY()] != '@') ){)
+                        valid=false;
+                    }
+                    i++;
+                }
+            }
+            return valid;
         }
-		throw new InvalidMovementException();
 	}
     
     
