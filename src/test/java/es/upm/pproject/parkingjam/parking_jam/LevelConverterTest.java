81c4bb3338d556b1c777a2cfe3e874a86915417d
MORILLA SORLÍ, DAVID <david.morilla@alumnos.upm.es>
2024-06-17 15:22:16 +0200

    Separating tests to have a maximum of 2 asserts per test

--- src/test/java/es/upm/pproject/parkingjam/parking_jam/LevelConverterTest.java
+++ src/test/java/es/upm/pproject/parkingjam/parking_jam/LevelConverterTest.java
@@ -51,15 +51,57 @@ class LevelConverterTest {
 				Car actualCar = map.get(a);
 				assertNotNull(actualCar, "Car with symbol '" + a + "' is missing in actual map");
 				assertEquals(expectedCar.getSymbol(), actualCar.getSymbol(), "Symbol mismatch for car with symbol '" + a + "'");
+				}
+
+		} catch (Exception e) {} 
+	}
+	
+@Test
+	void convertOKLevelTest2() {
+		try { 
+			char[][] board = lr.readMap(-1,false);
+			Map<Character, Car>	map = lc.convertLevel(board);
+			Map<Character, Car> expectedMap = new HashMap<>();
+			expectedMap.put('a', new Car('a', 1, 1, 2, 'H'));
+			expectedMap.put('b', new Car('b', 3, 1, 3, 'H'));
+			expectedMap.put('c', new Car('c', 6, 1, 2, 'V'));
+			expectedMap.put('d', new Car('d', 1, 3, 3, 'V'));
+			expectedMap.put('*', new Car('*', 4, 2, 2, 'V'));
+			expectedMap.put('f', new Car('f', 3, 4, 3, 'H'));
+			expectedMap.put('e', new Car('e', 2, 5, 2, 'V'));
+			expectedMap.put('g', new Car('g', 4, 6, 3, 'H'));
+			for (char a : expectedMap.keySet()) {
+				Car expectedCar = expectedMap.get(a);
+				Car actualCar = map.get(a);
 				assertEquals(expectedCar.getCoordinates().getX(), actualCar.getCoordinates().getX(), "X coordinate mismatch for car with symbol '" + a + "'");
 				assertEquals(expectedCar.getCoordinates().getY(), actualCar.getCoordinates().getY(), "Y coordinate mismatch for car with symbol '" + a + "'");
+			}
+
+		} catch (Exception e) {} 
+	}
+@Test
+	void convertOKLevelTest3() {
+		try { 
+			char[][] board = lr.readMap(-1,false);
+			Map<Character, Car>	map = lc.convertLevel(board);
+			Map<Character, Car> expectedMap = new HashMap<>();
+			expectedMap.put('a', new Car('a', 1, 1, 2, 'H'));
+			expectedMap.put('b', new Car('b', 3, 1, 3, 'H'));
+			expectedMap.put('c', new Car('c', 6, 1, 2, 'V'));
+			expectedMap.put('d', new Car('d', 1, 3, 3, 'V'));
+			expectedMap.put('*', new Car('*', 4, 2, 2, 'V'));
+			expectedMap.put('f', new Car('f', 3, 4, 3, 'H'));
+			expectedMap.put('e', new Car('e', 2, 5, 2, 'V'));
+			expectedMap.put('g', new Car('g', 4, 6, 3, 'H'));
+			for (char a : expectedMap.keySet()) {
+				Car expectedCar = expectedMap.get(a);
+				Car actualCar = map.get(a);
 				assertEquals(expectedCar.getLength(), actualCar.getLength(), "Length mismatch for car with symbol '" + a + "'");
 				assertEquals(expectedCar.getOrientation(), actualCar.getOrientation(), "Orientation mismatch for car with symbol '" + a + "'");
 			}
 
 		} catch (Exception e) {} 
 	}
-	
 	@Test
 	void convertLevelWithNoExitsTest() {
 		char[][] board = lr.readMap(-2,false);
